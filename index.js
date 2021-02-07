33df9f49ac301f9ae275d9fb320cd0e650a5c6c1
hurek <rudrobinrood@gmail.com>
2021-02-07 20:36:38 +0300

    Add: Mongoose + course model

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const Handlebars = require('handlebars');
 const exphbs = require('express-handlebars');
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
 const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
@@ -10,7 +13,8 @@ const app = express();
 
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
 })
 
 app.engine('hbs', hbs.engine);
@@ -26,6 +30,19 @@ app.use('/card', cardRoutes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+async function start() {
+  try {
+    const url = `mongodb+srv://hurek:4qfPsODsdcq00Xh9@express-js-store.nx0fw.mongodb.net/express-js-store`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false})
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+  })
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+start()
