d5b88a19c19e91223063096551c29174159a5f76
hurek <rudrobinrood@gmail.com>
2021-02-08 00:43:13 +0300

    Add: order functionality

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const homeRoutes = require('./routes/home');
 const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const cartRoutes = require('./routes/cart');
+const ordersRoutes = require('./routes/orders');
 const User = require('./models/user');
 
 const app = express();
@@ -38,6 +39,7 @@ app.use('/', homeRoutes);
 app.use('/add', addRoutes);
 app.use('/courses', coursesRoutes);
 app.use('/cart', cartRoutes);
+app.use('/orders', ordersRoutes);
 
 const PORT = process.env.PORT || 3000;
 
